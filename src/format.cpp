38db3010e8e7655cd0ec35a6fd0dae3759244c95
sol981 <kenny981cc@gmail.com>
2024-02-18 11:37:54 +0700

    [FORMAT] improve elapsed time format

--- src/format.cpp
+++ src/format.cpp
@@ -9,9 +9,30 @@ using std::string;
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) { 
+    string elapsedTime{};
     int h = seconds/3600;
     int m = (seconds - h*3600)/60;
     int s = seconds - h*3600 - m*60;
 
-    return std::to_string(h) + "::" + std::to_string(m) + "::" + std::to_string(s);
+    if(h < 10) {
+        elapsedTime += "0" + std::to_string(h);
+    }
+    else{
+        elapsedTime += std::to_string(h);
+    }
+    elapsedTime += "::";
+    if(m < 10) {
+        elapsedTime += "0" + std::to_string(m);
+    }
+    else{
+        elapsedTime += std::to_string(m);
+    }
+    elapsedTime += "::";
+    if(s < 10) {
+        elapsedTime += "0" + std::to_string(s);
+    }
+    else{
+        elapsedTime += std::to_string(s);
+    }
+    return elapsedTime;
 }

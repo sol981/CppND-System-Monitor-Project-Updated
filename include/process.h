86f55d62cd87cf111c4a399a8f5efe90f4ab2049
sol981 <kenny981cc@gmail.com>
2024-03-09 09:21:45 +0700

    [PROCESS] update cpu utilization

--- include/process.h
+++ include/process.h
@@ -12,7 +12,8 @@ public:
   int Pid();                               // TODO: See src/process.cpp
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
+  float CpuUtilization() const;
+  void CpuUtilization(long active_ticks, long system_ticks);
   std::string Ram();                       // TODO: See src/process.cpp
   long int UpTime();                       // TODO: See src/process.cpp
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
@@ -20,6 +21,10 @@ public:
   // TODO: Declare any necessary private members
  private:
     int pid_;
+    int cpu_;
+    long cached_active_ticks_{0};
+    long cached_idle_ticks_{0};
+    long cached_system_ticks_{0};
 };
 
 #endif
